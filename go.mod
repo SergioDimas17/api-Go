8407738c4406ba449d86a137233744c3d9e32fc2
SergioDimas17 <sergiodimas1117@gmail.com>
2024-01-17 14:27:04 -0500

    cambios

--- go.mod
+++ go.mod
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
-module example/webservicegin
-=======
+
 module github.com/SergioDimas17/webservicegin
->>>>>>> 26d9317 (cambios)
 
 go 1.19
 
 require (
-<<<<<<< HEAD
-=======
 	github.com/SergioDimas17/controllers v0.0.0-20240116174843-a3de07a731a5
->>>>>>> 26d9317 (cambios)
 	github.com/bytedance/sonic v1.10.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
-import (
+
+import
 	"github.com/gin-gonic/gin"
-<<<<<<< HEAD
-	"github.com/SergioDimas17/api-go/controllers"
-=======
 	"github.com/SergioDimas17/controllers"
->>>>>>> 26d9317 (cambios)
-)
+
+
 
 func main() {
 	router := gin.Default()
@@ -15,8 +13,6 @@ func main() {
 	router.POST("/formulario", controllers.PostFormulario)
 	router.GET("/formulario/:id", controllers.GetFormularioByID)
 	router.Run("localhost:8080")
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> 26d9317 (cambios)
+
